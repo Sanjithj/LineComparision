40269d3a450b5a7dd61bd3bb5ea10491464539b2
Sanjithj <sanjithj591997@gmail.com>
2022-08-16 12:08:59 +0530

    Added: cartision system

--- Line.java
+++ Line.java
@@ -1,9 +1,22 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Line {
 
 		public static void main(String[] args) {
 			
 			System.out.println("Welcome to line comparision");
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter x1= ");
+			double x1=sc.nextDouble();
+			System.out.println("Enter y1= ");
+			double y1=sc.nextDouble();
+			System.out.println("Enter x2= ");
+			double x2=sc.nextDouble();
+			System.out.println("Enter y2= ");
+			double y2=sc.nextDouble();
+			double length=  Math.sqrt(Math.pow((x2 - x1),2)+Math.pow((y2- y1),2));
+			System.out.println("Length of the line= "+length);
 		}
 }
