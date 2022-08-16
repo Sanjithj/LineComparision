8f41f1d85c3df32678a606e2d905e46582f08eab
Sanjithj <sanjithj591997@gmail.com>
2022-08-16 12:13:34 +0530

    Added: equal or not

--- Line.java
+++ Line.java
@@ -16,7 +16,24 @@ public class Line {
 			double x2=sc.nextDouble();
 			System.out.println("Enter y2= ");
 			double y2=sc.nextDouble();
-			double length=  Math.sqrt(Math.pow((x2 - x1),2)+Math.pow((y2- y1),2));
-			System.out.println("Length of the line= "+length);
-		}
+			double line1=  Math.sqrt(Math.pow((x2 - x1),2)+Math.pow((y2- y1),2));
+			System.out.println("Length of the line1 "+line1);
+			
+
+			System.out.println("Enter p1: ");
+			int p1= sc.nextInt();
+			System.out.println("Enter q1: ");
+			int q1=sc.nextInt();
+			System.out.println("Enter p2: ");
+			int p2= sc.nextInt();
+			System.out.println("Enter q2: ");
+			int q2= sc.nextInt();
+			double Line2= Math.sqrt((p2-p1)*(p2-p1)+(q2-q1)*(q2-q1));
+			System.out.println("length of Line2: "+Line2);
+			//boolean ans = Line1.equals(Line2);
+			if(line1==Line2)
+				System.out.println("Both lines are equal");
+			else
+				System.out.println("Both lines are not equal");
+	    	}
 }
